90d2436d7108426dcbfc4ad51fce424d24612093
isha-mg <mgisha2001380@gmail.com>
2023-10-08 10:51:13 +0530

    adding all files

---
+++ src/layouts/home/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+  render() {
+    return(
+      <main className="container">
+        <div className="pure-g">
+          <div className="pure-u-1-1">
+            <h1>SVNIT DOTSLASH Hackathon</h1>
+            <h2>Real Estate Mapping and Asset Tokenization</h2>
+            <img className="image" src="https://cdn-images-1.medium.com/max/1600/1*TKdUJZ3JFMBpm3zmPbm9mQ.png"  height="400" width="950" />
+          </div>
+        </div>
+      </main>
+    )
+  }
+}
+
+export default Home
